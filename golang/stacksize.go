99a5865d3438df2a21c045830c176f26db764008
funkygao <funky.gao@gmail.com>
2015-03-18 15:50:48 +0800

    in golang, stack size grows at rate of *2

--- golang/stacksize.go
+++ golang/stacksize.go
@@ -1,4 +1,9 @@
 // Test how big can the stack of a go routine be.
+// runtime/proc.go: if 64OS maxstacksize=1GB else maxstacksize=250MB
+//
+// Allocate a bigger segment and move the stack:
+// oldsize = gp->stack.hi - gp->stack.lo;
+// newsize = oldsize * 2;
 package main
 
 func main() {

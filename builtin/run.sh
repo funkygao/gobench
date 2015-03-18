a911b1b4524a5a6e055bced34b08111cdc1ec36f
funkygao <funky.gao@gmail.com>
2015-03-18 20:22:38 +0800

    benchmark with all cpu's is time consuming

--- builtin/run.sh
+++ builtin/run.sh
@@ -3,10 +3,10 @@ echo ========================
 echo golang builtin benchmark
 echo ========================
 go test -benchmem -cpu 1,2,4 -test.bench=".*" fmt
-go test -benchmem -cpu 1,2,4 -test.bench=".*" bytes
-go test -benchmem -cpu 1,2,4 -test.bench=".*" strconv
-go test -benchmem -cpu 1,2,4 -test.bench=".*" regexp
-go test -benchmem -cpu 1,2,4 -test.bench=".*" strings
+go test -benchmem  -test.bench=".*" bytes
+go test -benchmem  -test.bench=".*" strconv
+go test -benchmem  -test.bench=".*" regexp
+go test -benchmem  -test.bench=".*" strings
 
 echo
 echo

8d0f3ecfbcde2e7275d10330428af6d1736d77f5
funkygao <funky.gao@gmail.com>
2015-03-20 09:05:22 +0800

    refactor

--- builtin/benchmark_io.go
+++ builtin/benchmark_io.go
@@ -3,12 +3,28 @@ package main
 import (
 	"github.com/funkygao/gobench/util"
 	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func main() {
 	b := testing.Benchmark(benchmarkIoReadFull)
 	util.ShowBenchResult("io.ReadFull", b)
+	b = testing.Benchmark(benchmarkReadFile)
+	util.ShowBenchResult("iouti.ReadFile", b)
+}
+
+func benchmarkReadFile(b *testing.B) {
+	const F = "benchmark_io.go"
+
+	for i := 0; i < b.N; i++ {
+		ioutil.ReadFile(F)
+	}
+
+	f, _ := os.Open(F)
+	st, _ := f.Stat()
+	b.SetBytes(st.Size())
 }
 
 func benchmarkIoReadFull(b *testing.B) {
